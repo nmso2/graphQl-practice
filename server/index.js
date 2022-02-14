7ca0be9f81af67e076d7e1ed6494c36797cc327f
nmso2 <nmsubho@gmail.com>
2022-02-14 10:43:57 +0600

    add data

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 mongoose.connect(
-  `mongodb+srv://mydbuser:4vVAbtPAEIF8oKjQ@cluster0.sjr78.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  `mongodb+srv://graphQLPracticeUser:j71p7z32Rfzb2Ris@cluster0.0om44.mongodb.net/graphQLPractice?retryWrites=true&w=majority`
 );
 mongoose.connection.once("open", () => {
   console.log("connected to DB");
