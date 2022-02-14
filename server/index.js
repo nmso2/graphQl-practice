4760ff6e7ecb9c6687beb41cfe6f8b35a170c427
nmso2 <nmsubho@gmail.com>
2022-02-14 17:53:10 +0600

    frontend setup

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   `mongodb+srv://graphQLPracticeUser:j71p7z32Rfzb2Ris@cluster0.0om44.mongodb.net/graphQLPractice?retryWrites=true&w=majority`
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,6 +8,7 @@ const {
   GraphQLID,
   GraphQLInt,
   GraphQLList,
+  GraphQLNonNull,
 } = graphql;
 const _ = require("lodash");
 
@@ -98,8 +99,8 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve(parent, args) {
         let author = new Author({
@@ -112,9 +113,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
         let book = new Book({
